686228610e361038331e827e7bb317cf141076af
Rcan3624 <55251953+Rcan3624@users.noreply.github.com>
2024-11-17 23:41:38 -0500

    Add files via upload
    TestCode2.java was placed outside the src folder since I didn't have time to figure out how to get the program to read the file when it is inside the src folder

---
+++ TestCode2.java
@@ -0,0 +1,21 @@
+// Couldn't get the Java file writer to create a file inside the src folder. Just move this manually when you want to run the code
+
+
+/*
+Test comments
+"abstract", "assert", "boolean",
+"break", "byte", "case", "catch", "char", "class", "const",
+"continue", "default", "do", "double", "else", "enum",
+ */
+
+// "if", "implements", "import", "instanceof", "int"
+
+public class TestCode2 {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
